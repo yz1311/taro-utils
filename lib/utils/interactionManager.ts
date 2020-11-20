8bc8a01f9a0d93d3e45b13305bb85cb221680f98
yangzhao <yz1311@gmail.com>
2020-11-20 14:58:30 +0800

    fix

--- lib/utils/interactionManager.ts
+++ lib/utils/interactionManager.ts
@@ -5,6 +5,16 @@ const InteractionManager = {
         setTimeout(()=>{
             task&&task();
         }, 150);
+    },
+    /**
+     * 延迟执行任务
+     * @param task 要执行的任务
+     * @param time 延迟时间，默认为2500ms
+     */
+    delay: (task?: ()=>void, time = 2500) => {
+        setTimeout(()=>{
+            task&&task();
+        }, time);
     }
 };
 
