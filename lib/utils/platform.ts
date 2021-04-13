963b6601e92741e5027318704722ab1b7a6f524c
yz1311 <yz1311@gmail.com>
2021-04-13 13:31:51 +0800

    修复h5下报错的问题

--- lib/utils/platform.ts
+++ lib/utils/platform.ts
@@ -4,12 +4,13 @@ const Platform = {
     OS: (()=>{
         let systemInfo = Taro.getSystemInfoSync();
         //虽然说网上说的是模拟器获取的system是devtools,但是实际测试 iOS 10.0.1
+        //h5端返回的system为null
         let system = systemInfo.system?.toLowerCase();
-        if(system.indexOf('ios')>=0) {
+        if(system?.indexOf('ios')>=0) {
             return 'ios';
-        } else if(system.indexOf('android')>=0) {
+        } else if(system?.indexOf('android')>=0) {
             return 'android';
-        } else if(system.indexOf('devtools')>=0 || process.env.TARO_ENV === 'h5') {
+        } else if(system?.indexOf('devtools')>=0 || process.env.TARO_ENV === 'h5') {
             return 'web';
         } else {
             return system;
