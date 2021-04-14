0c9b7939f1eda88cf90e503fd324d97102a4ad1d
yz1311 <yz1311@gmail.com>
2021-04-14 15:56:54 +0800

    修复DeviceEventEmitter

--- lib/utils/deviceEventEmitter.ts
+++ lib/utils/deviceEventEmitter.ts
@@ -3,12 +3,12 @@ import {EmitterSubscription} from "../../types";
 
 
 const DeviceEventEmitter =  {
-    addListener: (eventName: string, params: any) => {
+    addListener: (eventName: string | symbol, params: any) => {
         Taro.eventCenter.on(eventName, params);
         return {
             eventType: eventName,
             remove: () => {
-                Taro.eventCenter.off('eventName');
+                Taro.eventCenter.off(eventName, params);
             }
         } as EmitterSubscription;
     },
